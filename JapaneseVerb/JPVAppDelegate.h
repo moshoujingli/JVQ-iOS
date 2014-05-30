7b0ce032df9e60255d6a77682867dad71c7ac510
BiXiaopeng <bixiaopeng2007@gmail.com>
2014-05-30 23:42:52 +0900

    init import

---
+++ JapaneseVerb/JPVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JPVAppDelegate.h
+//  JapaneseVerb
+//
+//  Created by BiXiaopeng on 14-5-30.
+//  Copyright (c) 2014年 BiXiaopeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JPVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JapaneseVerb/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JapaneseVerb
+//
+//  Created by BiXiaopeng on 14-5-30.
+//  Copyright (c) 2014年 BiXiaopeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JPVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JPVAppDelegate class]));
+    }
+}
